83217d2e03e534eb0e22b05be58df48c11b35b03
Giovanni Gargiulo <gargiulo.gianni@gmail.com>
2021-10-22 11:06:26 +0100

    version and tags

--- upload-spec.sh
+++ upload-spec.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
-GIT_TAG=$(git describe --abbrev=0)
+GIT_TAG=$(git describe --tags --abbrev=0)
 
 apibuilder upload stakeboard stakeboard-labels-lib .apibuilder/stakeboard-labels-lib.json --version ${GIT_TAG}
